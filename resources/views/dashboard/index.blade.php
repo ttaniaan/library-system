275810544c23b90779df00c1c91664f60d4d0b3a
ttaniaan <2410631250076@student.unsika.ac.id>
2026-09-15 20:22:42 +0700

    Latihan 1 Dashboard

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,2 @@
+<h1>Library System Dashboard</h1>
+<p>Selamat datang di Sistem Informasi Perpustakaan</p>
--- routes/web.php
+++ routes/web.php
@@ -4,28 +4,34 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\MembersController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/books', [BookController::class, 'index']);
 
 // Router::get('/books, function () {
 //      // return 'Daftar Buku';
 //      return view('books.index');    
 // });
 
-Route::get('/books', [BookController::class, 'index']);
+Route::get('/categories', [CategoriesController::class, 'index']);
 
 // Router::get('/categories, function () {
 //      // return 'Daftar Categories';
 //      return view('categories.index');    
 // });
 
-Route::get('/categories', [CategoriesController::class, 'index']);
-
+Route::get('/members', [MembersController::class, 'index']);
 // Router::get('/members, function () {
 //      // return 'Daftar Members';
 //      return view('members.index');    
 // });
 
-Route::get('/members', [MembersController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index']);
+// Router::get('/dashboard, function () {
+//      // return 'Dashboard';
+//      return view('dashboard.index');    
+// });

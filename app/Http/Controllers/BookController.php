6d7129aa1a457759aae1ff968aae6433b3cf6f3e
ttaniaan <2410631250076@student.unsika.ac.id>
2026-09-15 21:40:06 +0700

    Latihan 2 Route Parammeter

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -9,4 +9,8 @@ class BookController extends Controller
     public function index() {
         return view('books.index');
     }
+
+    public function show($id) {
+        return 'ID Buku: ' . $id;
+    }
 }
